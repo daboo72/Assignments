a9d5705cf78217ccde157638aace3e0ecc456214
daboo72 <69588737+daboo72@users.noreply.github.com>
2020-08-17 16:21:42 +0530

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+st="NITIN"
+st2=(st[::-1])
+print(st==st2)
+
+
+# In[30]:
+
+
+x="argentian..negratina"
+print(x[::-1])
+print(len(x))
+x1=(len(x))
+x2=x1//2
+print(x2)
+print(x[x1:x2:-1])
+
+
+# In[33]:
+
+
+str="i am from Bangalore"
+print(str.startswith("i"))
+print(str.endswith("e"))
+print(str.capitalize())
+
+
+# In[ ]:
+
+
+
+
