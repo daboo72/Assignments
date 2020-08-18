bf3cfc927bbe532b995f34b408a80f121873dfac
daboo72 <69588737+daboo72@users.noreply.github.com>
2020-08-18 16:16:36 +0530

    Add files via upload

---
+++ Assignment3.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+#
+
+
+# In[21]:
+
+
+#
+
+
+# In[23]:
+
+
+list=["foo","bar","baz"]
+list[1]="run"
+print(list)
+print(list[1])
+
+
+# In[30]:
+
+
+a=[]
+print(type(a))
+print(a)
+
+
+# In[34]:
+
+
+list2=["python",'java',"123",10]
+print(len(list2))
+
+
+# In[39]:
+
+
+list3=list2.copy()
+print(list3)
+
+
+# In[40]:
+
+
+l1=[12,15,17,19,30,45,10,56,95,10]
+print(l1.count(10))
+
+
+# In[ ]:
+
+
+
+
